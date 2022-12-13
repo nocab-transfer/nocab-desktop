356d8f448d2032f63db003526e968ded578a0bea
Berke Emin Kabagöz <berkekbgz@gmail.com>
2022-12-13 17:31:31 +0300

    Fix: Finder doesnt work properly when port changed

--- lib/custom_widgets/device_finder_bloc/device_finder_cubit.dart
+++ lib/custom_widgets/device_finder_bloc/device_finder_cubit.dart
@@ -7,6 +7,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nocab_desktop/custom_widgets/device_finder_bloc/device_finder_state.dart';
 import 'package:nocab_desktop/models/deviceinfo_model.dart';
 import 'package:nocab_desktop/services/server/server.dart';
+import 'package:nocab_desktop/services/settings/settings.dart';
 
 class DeviceFinderCubit extends Cubit<DeviceFinderState> {
   DeviceFinderCubit() : super(const NoDevice());
@@ -22,7 +23,7 @@ class DeviceFinderCubit extends Cubit<DeviceFinderState> {
       List<DeviceInfo> devices = [];
       for (int i = 1; i < 255; i++) {
         try {
-          Socket socket = await Socket.connect('$baseIp.$i', 62192, timeout: const Duration(milliseconds: 15));
+          Socket socket = await Socket.connect('$baseIp.$i', SettingsService().getSettings.finderPort, timeout: const Duration(milliseconds: 15));
           Uint8List data = await socket.first.timeout(const Duration(seconds: 5));
           if (data.isNotEmpty && socket.remoteAddress.address != Server().selectedIp.address) {
             devices.add(DeviceInfo.fromJson(json.decode(utf8.decode(base64.decode(utf8.decode(data))))));
